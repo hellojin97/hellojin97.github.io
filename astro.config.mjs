2023ba8c069766b6668fe3d17e060e76f1f46861
김현진 <ilhj1228@gmail.com>
2026-08-27 01:05:15 +0900

    Switch site URL to Cloudflare Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://hellojin97.github.io/",
+	site: "https://hellojin97.pages.dev/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
